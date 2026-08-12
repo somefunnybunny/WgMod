6d29cb2752a9a80ef9f8373ed1bb5d1c09e2ddbe
somefunnybunny <lagotemplar@gmail.com>
2026-08-12 11:25:05 -0400

    Add biome enemy debug spawner

--- Content/Items/Debug/EnemySpawnTestItems.cs
+++ Content/Items/Debug/EnemySpawnTestItems.cs
@@ -1,6 +1,7 @@
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using WgMod.Content.NPCs.BiomeVariants;
 using WgMod.Content.NPCs.Caverns;
 
 namespace WgMod.Content.Items.Debug;
@@ -36,7 +37,6 @@ public class TestFeedingManEater : ModItem
         int anchorX = startX;
         int anchorY = startY;
 
-        // Find nearby solid terrain so the vanilla Man Eater AI has a legitimate tether tile.
         bool foundAnchor = false;
         for (int radius = 0; radius <= 12 && !foundAnchor; radius++)
         {
@@ -73,6 +73,67 @@ public class TestFeedingManEater : ModItem
             ai1: anchorY,
             Target: player.whoAmI
         );
+        return true;
+    }
+}
+
+public class TestBiomeVariantEnemy : ModItem
+{
+    int _nextEnemy;
+
+    public override string Texture => "WgMod/Content/Items/WeightManipulator";
+
+    public override void SetStaticDefaults()
+    {
+        Item.ResearchUnlockCount = 1;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.width = 20;
+        Item.height = 20;
+        Item.maxStack = 1;
+        Item.useStyle = ItemUseStyleID.HoldUp;
+        Item.useTime = 10;
+        Item.useAnimation = 10;
+        Item.noMelee = true;
+        Item.UseSound = SoundID.Item4;
+    }
+
+    public override bool? UseItem(Player player)
+    {
+        if (player.whoAmI != Main.myPlayer || Main.netMode == NetmodeID.MultiplayerClient)
+            return null;
+
+        int[] roster =
+        {
+            ModContent.NPCType<StuffingSlime>(),
+            ModContent.NPCType<GorgeWorm>(),
+            ModContent.NPCType<SyrupAntlion>(),
+            ModContent.NPCType<BanquetSkeleton>(),
+            ModContent.NPCType<HoneyHornetQueen>(),
+            ModContent.NPCType<Bloatfish>(),
+            ModContent.NPCType<CreamSlushie>(),
+            ModContent.NPCType<FurnaceImp>(),
+            ModContent.NPCType<PossessedBuffet>(),
+            ModContent.NPCType<BlobLeech>(),
+            ModContent.NPCType<MirrorNymph>(),
+        };
+
+        int type = roster[_nextEnemy % roster.Length];
+        _nextEnemy = (_nextEnemy + 1) % roster.Length;
+
+        int index = NPC.NewNPC(
+            player.GetSource_Misc("TestBiomeVariantEnemy"),
+            (int)player.Center.X + player.direction * 180,
+            (int)player.Center.Y - 32,
+            type,
+            Target: player.whoAmI
+        );
+
+        if (index >= 0 && index < Main.maxNPCs)
+            Main.NewText($"Spawned {Main.npc[index].FullName} ({_nextEnemy}/{roster.Length} next in cycle).", 180, 220, 255);
+
         return true;
     }
 }
