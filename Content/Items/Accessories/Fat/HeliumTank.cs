ff5e5315d213d1535d605cae2ee615daafd69fb8
somefunnybunny <lagotemplar@gmail.com>
2026-08-11 21:01:33 -0400

    Make Helium Tank pump Bloated while equipped

--- Content/Items/Accessories/Fat/HeliumTank.cs
+++ Content/Items/Accessories/Fat/HeliumTank.cs
@@ -2,6 +2,7 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 using WgMod.Common.Players;
+using WgMod.Content.Buffs.Debuffs;
 
 namespace WgMod.Content.Items.Accessories.Fat;
 
@@ -29,6 +30,41 @@ public class HeliumTank : ModItem
         float immobility = wg.Weight.ClampedImmobility;
         _gravity.Lerp(immobility);
         player.gravity *= _gravity;
+
+        if (player.TryGetModPlayer(out HeliumTankPlayer heliumTank))
+            heliumTank.Equipped = true;
+    }
+}
+
+public class HeliumTankPlayer : ModPlayer
+{
+    const int PumpInterval = 60;
+    const int BloatedTimePerPump = 60 * 3;
+
+    int _pumpTimer;
+
+    public bool Equipped { get; set; }
+
+    public override void ResetEffects()
+    {
+        Equipped = false;
+    }
+
+    public override void PostUpdateEquips()
+    {
+        if (!Equipped || Player.dead)
+        {
+            _pumpTimer = 0;
+            return;
+        }
+
+        _pumpTimer++;
+        if (_pumpTimer < PumpInterval)
+            return;
+
+        _pumpTimer = 0;
+        if (Player.TryGetModPlayer(out BloatedPlayer bloated))
+            bloated.ApplyBloated(BloatedTimePerPump);
     }
 }
 
